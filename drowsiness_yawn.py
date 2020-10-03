46f2e781fe1aa8441de25104a08465b63e809de6
Kunal Darwesh <57343972+KunalDarwesh@users.noreply.github.com>
2020-10-03 10:56:14 +0530

    Update drowsiness_yawn.py

--- drowsiness_yawn.py
+++ drowsiness_yawn.py
@@ -95,6 +95,7 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     #rects = detector(gray, 0)
+    #Credits:-SPARKLERS : We Are The Makers
     rects = detector.detectMultiScale(gray, scaleFactor=1.1,
 		minNeighbors=5, minSize=(30, 30),
 		flags=cv2.CASCADE_SCALE_IMAGE)
